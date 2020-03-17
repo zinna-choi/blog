80310a73dd0fb5759ea87452fee1ba0312f4b5ee
jieunchoi <jehy0807@gmail.com>
2020-03-17 09:26:10 +0900

    express 를 이용한 로컬서버구축

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require( 'express' )
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send(`Response Complate`);
+  res.send( 'welcome to jieuns' );
 })
+
 
 app.listen(PORT, () => {
   console.log(`Server On : http://localhost:${PORT}/`);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      host : '',
+    }
+  }
+  componentDidMount() {
+    this._getHost();
+  }
+  _getHost = async() => {
+    const res = await axios.get('/api/host');
+    this.setState({ host : res.data.host })
+  }
+
+  render(){
+    return(
+      <div className ='App'>
+        <h3> Welcome to <u> {this.state.host} </u> Blog!</h3>
+      </div>
+    )
+  }
 }
 
 export default App;

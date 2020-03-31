3c529cc5ddef9c3bb4b0e9ae76c2d28ab5200ab7
thtjwls <thtjwls@gmail.com>
2020-03-31 21:31:36 +0900

    보안 정보를 커밋에 포함하면 안됩니다.
    보편적으로, access_key 와 secret_key 커밋에 공개되도록 하면 안됩니다.
    공개 되었을 경우, 애플리케이션이 타인에게 노출되어 악의적으로 사용 될 수 있습니다.
    
    이 이슈를 처리 하기 위해서는, 민감한 정보들은 따로 관리하세요.
    
    node.js 에서는 이미 dotenv 라는 라이브러리를 제공합니다.
    
    1. dotenv 를 dependancy 에 포함하세요.
    2. 코드 실행시, 최대한 상단에 `require('dotenv').config()` 코드를 포함하세요.
    3. `.env` 파일 을 project root 에 포함시키고 프로퍼티를 추가하세요.
    
    _.env_
    
    .env 파일의 작성은 다음과 같이 합니다.
    
    ```bash
    EXAMPLE=test
    ```
    
    이렇게 작성했다면, 애플리케이션에서는
    
    ```bash
    process.env.EXAMPLE
    # test
    ```
    
    이렇게 사용 할 수 있습니다.
    
    추가적으로, create-react-app 에서는 prefix 로 REACT_APP 이 있어야 합니다.
    
    _.env_
    
    ```bash
    REACT_APP_EXAMPLE=test-react-app
    ```

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,8 +12,9 @@ app.get("/api/host", (req, res) => {
 
 app.get("/outh/token", (req, res) => {
   const tistory = "https://www.tistory.com/oauth/access_token";
-  const request = axios.post(`${tistory}?client_id=8d0b20b1fa5058e6df37d967f22474bf&client_secret=
-  8d0b20b1fa5058e6df37d967f22474bfc2796c5f38e8b94d9788e194849c17c0c9684496&redirect_uri=https://locallhost4000&code=1f7316020e477b19a84fb91a517ea915afb796745a489c68bab1bec9f5a5e7b853e6eba5&grant_type=authorization_code`);
+  const request = axios.post(
+    `${tistory}?client_id=${process.env.TISTORY_ACCESS_KEY}&client_secret=${process.env.TISTORY_SECRET_KEY}&redirect_uri=https://locallhost4000&code=1f7316020e477b19a84fb91a517ea915afb796745a489c68bab1bec9f5a5e7b853e6eba5&grant_type=authorization_code`
+  );
 
   request.then(({ data }) => {
     const access_token = data.access_token;
